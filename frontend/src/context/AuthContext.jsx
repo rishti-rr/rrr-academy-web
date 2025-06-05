8e95e1ece3fad3bd3e5375f2167c3c9f931c75b1
rishti-rr <rishti18cse.pstu.ac.bd>
2025-06-05 20:00:51 +0600

    faltu commit

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState, useContext } from 'react';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from "../Login/firebaseConfig"; 
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsub();
+  }, []);
+
+  const logout = () => {
+    signOut(auth);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, logout }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
