60121e2d4268ad8900d6bc3f2c942b8e0a206d57
rishti-rr <rishti18cse.pstu.ac.bd>
2025-06-21 23:29:38 +0600

    Test

--- backend/server.js
+++ backend/server.js
@@ -32,14 +32,14 @@ mongoose.connect(MONGO_URI)
 const bookRoutes = require("./routes/bookRoutes");
 const courseRoutes = require("./routes/courseRoutes");
 const adminRoutes = require('./routes/adminRoutes');
-const statsRoutes = require("./routes/stats");
+const statsRoutes = require("./routes/statsRoutes");
 const userRoutes = require("./routes/userRoutes");
 
-// app.use("/api/books", bookRoutes);
-// app.use("/api/courses", courseRoutes);
-// app.use('/api/admin', adminRoutes);
-// app.use("/api/stats", statsRoutes); 
-// app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/courses", courseRoutes);
+app.use('/api/admin', adminRoutes);
+app.use("/api/stats", statsRoutes); 
+app.use("/api/users", userRoutes);
 
 // Server Start
 const PORT = process.env.PORT || 5000;
